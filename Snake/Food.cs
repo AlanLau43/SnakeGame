b612f73ea70893b64e9b35e01bfaf0205d193302
AlanLau43 <62096744+AlanLau43@users.noreply.github.com>
2020-04-25 18:59:49 +0800

    Add files via upload

---
+++ Snake/Food.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Food
+    {
+        private Position food;
+        public Food()
+        {
+            Random randomNumbersGenerator = new Random();
+            food = new Position(randomNumbersGenerator.Next(0, 30),
+                    randomNumbersGenerator.Next(0, 120));
+
+        }
+
+        public Position Generate(int x, int y)
+        {
+            Position g = new Position(x,y);
+            return g;
+        }
+
+        public Position GetFood()
+        {
+            return food;
+        }
+    }
+}
