3eb660bce15eb546405f1e5b338cc05175d863a1
newbie2345 <62095948+newbie2345@users.noreply.github.com>
2020-04-25 19:01:09 +0800

    Add files via upload

---
+++ Snake/Obstacle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Obstacle
+    {
+        private List<Position> obstacles;
+        public Obstacle()
+        {
+            Random randomNumbersGenerator = new Random();
+            obstacles = new List<Position>()
+            {
+                new Position(randomNumbersGenerator.Next(0, 30),
+                            randomNumbersGenerator.Next(0, 120)),
+                new Position(randomNumbersGenerator.Next(0, 30),
+                            randomNumbersGenerator.Next(0, 120)),
+                new Position(randomNumbersGenerator.Next(0, 30),
+                            randomNumbersGenerator.Next(0, 120)),
+                new Position(randomNumbersGenerator.Next(0, 30),
+                            randomNumbersGenerator.Next(0, 120)),
+                new Position(randomNumbersGenerator.Next(0, 30),
+                            randomNumbersGenerator.Next(0, 120)),
+            };
+        }
+
+        public Position Generate(int x, int y)
+        {
+            Position ob = new Position(x,y);
+
+            return ob;
+        }
+
+        public List<Position> GetPositions()
+        {
+            return obstacles;
+        }
+
+        public void Add(Position p)
+        {
+            obstacles.Add(p);
+        }
+
+    }
+}
