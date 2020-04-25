7fc8fa70d906fa9de15074441ee09389697ec834
newbie2345 <62095948+newbie2345@users.noreply.github.com>
2020-04-25 19:02:17 +0800

    Add files via upload

---
+++ snake.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake
+    {
+        private Direction direction;
+        private Queue<Position> snakeElements;
+        public Snake()
+        {
+            direction = Direction.right;
+            snakeElements = new Queue<Position>();
+            for (int i = 0; i <=3; i++)
+            {
+                snakeElements.Enqueue(new Position(0, i));
+            }
+        }
+
+        public Position[] Directions()
+        {
+            Position[] directions = new Position[]
+            {
+                new Position(0, 1), // right
+                new Position(0, -1), // left
+                new Position(1, 0), // down
+                new Position(-1, 0), // up
+            };
+
+            return directions;
+        }
+
+        public Position Move_Snake(Position snakeHead)
+        {
+            
+            Position[] directions = Directions();
+            Position nextDirection = directions[(int)direction];
+            Position snakeNewHead = new Position(snakeHead.row + nextDirection.row,
+                    snakeHead.col + nextDirection.col);
+
+            if(snakeNewHead.col < 0)
+            {
+                snakeNewHead.col = Console.WindowWidth - 1;
+            }
+
+            if (snakeNewHead.row < 0)
+            {
+                snakeNewHead.row = Console.WindowHeight - 1;
+            }
+
+            if (snakeNewHead.row >= Console.WindowHeight)
+            {
+                snakeNewHead.row = 0;
+            }
+
+            if (snakeNewHead.col >= Console.WindowWidth)
+            {
+                snakeNewHead.col = 0;
+            }
+
+            return snakeNewHead;
+
+        }
+
+        public void updateSnake(Position p)
+        {
+            snakeElements.Enqueue(p);
+        }
+
+        public string SnakeHead()
+        {
+            if(direction == Direction.right)
+            {
+                return ">";
+            }else if(direction == Direction.left)
+            {
+                return "<";
+            }else if(direction == Direction.up)
+            {
+                return "^";
+            }
+            else
+            {
+                return "v";
+            }
+        }
+
+        public void SnakeBody()
+        {
+            foreach (Position position in snakeElements)
+            {
+                Console.SetCursorPosition(position.col, position.row);
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("*");
+            }
+        }
+
+        public Position Moving()
+        {
+            Position last = snakeElements.Dequeue();
+            return last;
+        }
+
+        public Queue<Position> GetSnakeElements()
+        {
+            return snakeElements;
+        }
+
+        public Direction GetDirection()
+        {
+            return direction;
+        }
+
+        public void SetDirection(Direction x)
+        {
+            direction = x;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
